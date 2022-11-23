afec2f475de55a61f48a5db5012c76b68af9fb83
admin <admin@admins-imac.gateway>
2022-11-23 20:46:12 +1100

    1st commit - Init project with dependencies

---
+++ src/main/java/com/udacity/hearttoheartflowers/HeartToHeartFlowersApplication.java
@@ -0,0 +1,12 @@
+package com.udacity.hearttoheartflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartToHeartFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartToHeartFlowersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/udacity/hearttoheartflowers/HeartToHeartFlowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.hearttoheartflowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartToHeartFlowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
